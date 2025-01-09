bbfca771c52394deeddf8cbcd3587857dec71589
mbalbino1997 <rmassimo.balbino@gmail.com>
2025-01-09 18:06:21 +0100

    add

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,26 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
 export default function HomePage() {
+    const [movies, setMovies] = useState([])
+    function fetchData() {
+        axios.get("http://localhost:3000/movies")
+            .then((res) => {
+                setMovies(res.data);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            })
+    }
+    useEffect(() => {
+        fetchData()
+    }, [])
     return (
-        <>
-            <h1>MOVIES</h1>
+        <><ul>
+            {movies.map((el, i) => (
+                <li key={i}>{el.title}</li>
+            ))}
+
+        </ul>
         </>
     )
 }
